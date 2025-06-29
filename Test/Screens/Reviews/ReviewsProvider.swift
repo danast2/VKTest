5ea79192de4b09572e73edb8614e51cde989c8ea
Daniil <danast_2@mail.ru>
2025-06-29 18:52:06 +0300

    refs with main queue

--- Test/Screens/Reviews/ReviewsProvider.swift
+++ Test/Screens/Reviews/ReviewsProvider.swift
@@ -8,36 +8,35 @@ final class ReviewsProvider {
     init(bundle: Bundle = .main) {
         self.bundle = bundle
     }
-
 }
 
-// MARK: - Internal
-
+// MARK: - Internal API
 extension ReviewsProvider {
 
     typealias GetReviewsResult = Result<Data, GetReviewsError>
 
     enum GetReviewsError: Error {
-
         case badURL
         case badData(Error)
-
     }
 
+    /// «Запрос» отзывов. Теперь выполняется асинхронно.
     func getReviews(offset: Int = 0, completion: @escaping (GetReviewsResult) -> Void) {
-        guard let url = bundle.url(forResource: "getReviews.response", withExtension: "json") else {
-            return completion(.failure(.badURL))
-        }
 
-        // Симулируем сетевой запрос - не менять
-        usleep(.random(in: 100_000...1_000_000))
+        DispatchQueue.global(qos: .userInitiated).async { [bundle] in
+            guard let url = bundle.url(forResource: "getReviews.response", withExtension: "json") else {
+                return DispatchQueue.main.async { completion(.failure(.badURL)) }
+            }
 
-        do {
-            let data = try Data(contentsOf: url)
-            completion(.success(data))
-        } catch {
-            completion(.failure(.badData(error)))
+            usleep(.random(in: 100_000...1_000_000))
+
+            do {
+                let data = try Data(contentsOf: url)
+                // ⬅️ Возвращаем результат НА ГЛАВНЫЙ ПОТОК
+                DispatchQueue.main.async { completion(.success(data)) }
+            } catch {
+                DispatchQueue.main.async { completion(.failure(.badData(error))) }
+            }
         }
     }
-
 }
